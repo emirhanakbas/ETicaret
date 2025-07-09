d0df820bcdb94f8bba5726be9ae0fc94fcba2d0d
Emirhan Akbaş <emirhanakbas@yahoo.com>
2025-07-09 17:31:49 +0300

    Add Entities and DbContext

---
+++ ETicaretAPI/ETicaretApp.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Persistence.Contexts {
+    public class ETicaretAPIDbContext : DbContext {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+ETicaretApp
+ETicaretApp
+ETicaretApp
